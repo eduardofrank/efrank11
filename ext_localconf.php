7f68f4c25837c2a5406b70d333e844f7eb28365f
eduardofrank <edfrank@gmail.com>
2023-02-18 02:37:18 +0000

    Form labels translation - translationFiles

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,7 +3,7 @@ defined('TYPO3') or die('Access denied.');
 /***************
  * Add default RTE configuration
  */
-/*$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['efrank11'] = 'EXT:efrank11/Configuration/RTE/Default.yaml';*/
+/*$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['efrank11'] = 'EXT:efrank11/Configuration/RTE/Full.yaml';*/
 
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['efrank11'] = 'EXT:efrank11/Configuration/RTE/MyFull.yaml';
 
